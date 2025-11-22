c9915ff48d243071113945a06a0b3c722427e96f
24520338-maker <24520338@gm.uit.edu.vn>
2025-11-22 15:26:01 +0700

    Commit các thay đổi Lab4 và cập nhật .gitignore

---
+++ LAB4/Menu.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace LAB4
+{
+    partial class Menu
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private Guna.UI2.WinForms.Guna2Button btn1;
+        private Guna.UI2.WinForms.Guna2Button btn2;
+        private Guna.UI2.WinForms.Guna2Button btn3;
+        private Guna.UI2.WinForms.Guna2Button btn4;
+        private Guna.UI2.WinForms.Guna2Button btn5;
+        private Guna.UI2.WinForms.Guna2Button btn6;
+        private Guna.UI2.WinForms.Guna2Button btn7;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        #region Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.btn1 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn2 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn3 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn4 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn5 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn6 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn7 = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.Transparent;
+            this.btn1.BorderRadius = 15;
+            this.btn1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.btn1.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.btn1.ForeColor = System.Drawing.Color.White;
+            this.btn1.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btn1.Location = new System.Drawing.Point(50, 50);
+            this.btn1.Name = "btn1";
+            this.btn1.ShadowDecoration.Depth = 10;
+            this.btn1.ShadowDecoration.Enabled = true;
+            this.btn1.Size = new System.Drawing.Size(220, 60);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Project 1";
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Transparent;
+            this.btn2.BorderRadius = 15;
+            this.btn2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.btn2.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.btn2.ForeColor = System.Drawing.Color.White;
+            this.btn2.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
+            this.btn2.Location = new System.Drawing.Point(300, 50);
+            this.btn2.Name = "btn2";
+            this.btn2.ShadowDecoration.Depth = 10;
+            this.btn2.ShadowDecoration.Enabled = true;
+            this.btn2.Size = new System.Drawing.Size(220, 60);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Project 2";
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.Transparent;
+            this.btn3.BorderRadius = 15;
+            this.btn3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            this.btn3.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.btn3.ForeColor = System.Drawing.Color.White;
+            this.btn3.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(43)))));
+            this.btn3.Location = new System.Drawing.Point(50, 130);
+            this.btn3.Name = "btn3";
+            this.btn3.ShadowDecoration.Depth = 10;
+            this.btn3.ShadowDecoration.Enabled = true;
+            this.btn3.Size = new System.Drawing.Size(220, 60);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "Project 3";
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.Transparent;
+            this.btn4.BorderRadius = 15;
+            this.btn4.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(89)))), ((int)(((byte)(182)))));
+            this.btn4.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.btn4.ForeColor = System.Drawing.Color.White;
+            this.btn4.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(68)))), ((int)(((byte)(173)))));
+            this.btn4.Location = new System.Drawing.Point(300, 130);
+            this.btn4.Name = "btn4";
+            this.btn4.ShadowDecoration.Depth = 10;
+            this.btn4.ShadowDecoration.Enabled = true;
+            this.btn4.Size = new System.Drawing.Size(220, 60);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "Project 4";
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.Transparent;
+            this.btn5.BorderRadius = 15;
+            this.btn5.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.btn5.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.btn5.ForeColor = System.Drawing.Color.White;
+            this.btn5.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(156)))), ((int)(((byte)(18)))));
+            this.btn5.Location = new System.Drawing.Point(50, 210);
+            this.btn5.Name = "btn5";
+            this.btn5.ShadowDecoration.Depth = 10;
+            this.btn5.ShadowDecoration.Enabled = true;
+            this.btn5.Size = new System.Drawing.Size(220, 60);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "Project 5";
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.Transparent;
+            this.btn6.BorderRadius = 15;
+            this.btn6.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(188)))), ((int)(((byte)(156)))));
+            this.btn6.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.btn6.ForeColor = System.Drawing.Color.White;
+            this.btn6.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(160)))), ((int)(((byte)(133)))));
+            this.btn6.Location = new System.Drawing.Point(300, 210);
+            this.btn6.Name = "btn6";
+            this.btn6.ShadowDecoration.Depth = 10;
+            this.btn6.ShadowDecoration.Enabled = true;
+            this.btn6.Size = new System.Drawing.Size(220, 60);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "Project 6";
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.Transparent;
+            this.btn7.BorderRadius = 15;
+            this.btn7.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(165)))), ((int)(((byte)(166)))));
+            this.btn7.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.btn7.ForeColor = System.Drawing.Color.White;
+            this.btn7.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(140)))), ((int)(((byte)(141)))));
+            this.btn7.Location = new System.Drawing.Point(175, 290);
+            this.btn7.Name = "btn7";
+            this.btn7.ShadowDecoration.Depth = 10;
+            this.btn7.ShadowDecoration.Enabled = true;
+            this.btn7.Size = new System.Drawing.Size(220, 60);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "Project 7";
+            // 
+            // Menu
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Menu";
+            this.Text = "7-Button Modern Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
