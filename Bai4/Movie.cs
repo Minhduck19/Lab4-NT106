69555b92068805ded476edf983451c95139793c3
MinDuck <ducvominh@suctremmt.com>
2025-11-23 15:26:54 +0700

    upcode bai 4

---
+++ Bai4/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bai4 
+{
+    public class Movie
+    {
+        public string Title { get; set; }       // Tên phim
+        public string ImageUrl { get; set; }    // Link ảnh poster
+        public string DetailUrl { get; set; }   // Link chi tiết để mở web
+        public string Genre { get; set; }       // Thể loại
+    }
+}
