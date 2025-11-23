2e5dbf3ae07d935a005e59130787ba4858ca85a0
MinDuck <ducvominh@suctremmt.com>
2025-11-23 16:46:37 +0700

    up code bai 4 moi

---
+++ Bai4/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bai4
+{
+    public class Movie
+    {
+        public string Title { get; set; }      
+        public string ImageUrl { get; set; }    
+        public string DetailUrl { get; set; }   
+        public string Genre { get; set; }      
+    }
+}
