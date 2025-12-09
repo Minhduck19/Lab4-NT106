090d3e34a8782001b127d60f3eca8d21f79b34f0
24520338-maker <24520338@gm.uit.edu.vn>
2025-12-09 08:43:06 +0700

    Client

--- Bai Test/Client.Designer.cs
+++ Bai Test/Client.Designer.cs
@@ -65,7 +65,6 @@
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(720, 255);
             this.dataGridView1.TabIndex = 2;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label2
             // 
